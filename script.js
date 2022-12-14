aaef5214ef8e07ad0abc31687a66df45589a5b6d
Dvoryashin <103428511+Dvoryashin@users.noreply.github.com>
2022-12-14 21:07:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function replace(){
+    var text = document.querySelector('#text').value;
+    document.querySelector('#duplicateField').innerHTML = text;
+}
+function resetInput(){
+    var text = document.querySelector('#text').value;
+    console.log(text);
+    document.querySelector('#text').value = "";
+    document.querySelector('#duplicateField').innerHTML = "";
+}
